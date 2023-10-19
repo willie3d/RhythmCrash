053f81df0ba09940458d7888a56953ede1b281c5
willie3d <www.willdoyle10@gmail.com>
2023-10-19 17:01:12 -0400

    Progress

--- Assets/Scripts/NoteObject.cs
+++ Assets/Scripts/NoteObject.cs
@@ -21,6 +21,8 @@ public class NoteObject : MonoBehaviour
             if (canBePressed)
             {
                 gameObject.SetActive(false);
+
+                GameManager.instance.NoteHit();
             }
         }
     }
@@ -36,6 +38,9 @@ public class NoteObject : MonoBehaviour
         if (other.tag == "Activator")
         {
             canBePressed = false;
+
+            GameManager.instance.NoteMiss();
+
         }
     }
 }
